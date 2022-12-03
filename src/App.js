a151b31046f3420a3894d15668731c5a3b725781
tan29121996 <tan29121996@gmail.com>
2022-12-03 14:20:58 +0800

    Transactions frontend
    Tan Si Yuan

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Transactions from './components/Transactions';
+import ScheduledTransactions from './components/ScheduledTransactions';
 
 function App() {
+  const [toggleState, setToggleState] = useState(1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ width: '100%', height: '100%' }}>
+      <div style={{ display: 'flex', flexDirection: 'row', width: 300 }}>
+        <div style={{ width: 100, height: 10, background: 'grey' }} onClick={() => setToggleState(1)}>Scheduled</div>
+        <div style={{ width: 100, height: 10, background: 'grey' }} onClick={() => setToggleState(2)}>Scheduled</div>
+      </div>
+      <Transactions {...{ setToggleState, toggleState }} />
+      <ScheduledTransactions {...{ setToggleState, toggleState }} />
     </div>
   );
 }
