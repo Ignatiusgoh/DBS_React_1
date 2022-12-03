5edee5a7063b8fda3f0ea92e9fb5ed7c6ec87046
Ignatiusgoh <ignatiusgoh97@gmail.com>
2022-12-03 14:44:42 +0800

    User Info
    Ignatius- User descriptions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Dropdown from './components/Dropdown';
+import UserImage from './components/Userimage';
+import Userinfo from './components/Userinfo';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserImage />
+      <Dropdown />
+      <Userinfo />
     </div>
   );
 }
