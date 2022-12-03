377c833edd55b66a7d03fccc8d29102465a8eb63
fishballwroe <108323989+fishballwroe@users.noreply.github.com>
2022-12-03 15:26:33 +0800

    Edits to Login
    Yuin Rui

---
+++ src/LoginPage.js
@@ -0,0 +1,32 @@
+const LoginPage = () => {
+    return (
+        <div class="content-body">
+            <div class="form-wrapper">
+                <form class="bg-white">
+                    <img src="https://logos-download.com/wp-content/uploads/2016/12/DBS_Bank_logo_logotype.png" alt="DBS Logo" />
+                        <h1 class="text-title">LOGIN </h1>
+                        <div class="field-group">
+                            <label class="label" for="txt-email">Email address</label>
+                            <input class="input" type="email" id="txt-email" name="email" placeholder="geraldtohdy@gmail.com" />
+                        </div>
+                        <div class="field-group">
+                            <label class="label" for="txt-password">Password</label>
+                            <input class="input" type="password" id="txt-password" name="password" placeholder="Enter password" />
+                            <a href="#forgot" class="link-forgot">Forgot?</a>
+                        </div>
+
+                        <div class="field-group">
+                            <input class="btn-submit" type="submit" value="Log In" />
+                        </div>
+                </form>
+
+                <div class="bg-grey">
+                    <a href="#register" class="link-register">Sign Up</a>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LoginPage;
